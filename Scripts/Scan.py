f19e1326c84ce21144b4eea9cd0a522f8d27dab4
caojian <1736665642@qq.com>
2024-02-23 15:31:39 +0800

    Update.

--- Scripts/Scan.py
+++ Scripts/Scan.py
@@ -7,50 +7,60 @@ import json
 from time import sleep
 
 
-def calculatefile_md5(content):
-    h = hashlib.md5()
-    h.update(content.encode())
-    return h.hexdigest()
+def calc_md5(content):
+    try:
+        h = hashlib.md5()
+        h.update(content.encode())
+        return h.hexdigest()
+    except Exception as e:
+        print(f"出现错误：{e}")
+        pass
 
 
 async def upload_file(file_md5, file_text):
-    conn = aiohttp.TCPConnector(ssl=False)
-    async with aiohttp.ClientSession(connector=conn) as session:
-        headers = {"Content-Type": "application/json"}
-        data = {"webshell_text": str(base64.b64encode(bytes(file_text, encoding='utf-8')).decode())}
-        async with session.get(url + "/api/v1/anonymous_see_file?apikey=" + apikey + "&md5=" + file_md5, json=data,
-                               headers=headers) as check_resp:
-            print("正在进行历史结果探测")
-            resp = await check_resp.text()
-            result = json.loads(resp)["result"]
-            if result != "":
-                print(f"云端结果已存在,云端扫描结果为：{result}")
-            else:
-                print("云端结果不存在,开始上传")
-                async with session.post(url + "/api/v1/anonymous_up_file?apikey=" + apikey + "&md5=" + file_md5,
-                                        json=data, headers=headers) as resp:
-                    print(resp.status)
-                    print(await resp.json())
-                    print("文件上传成功")
-                    print()
-                    for i in range(0, 10):
-                        print("正在等待云端扫描返回结果")
-                        async with session.get(url + "/api/v1/anonymous_see_file?apikey=" + apikey + "&md5=" + file_md5,
-                                               json=data, headers=headers) as upload_resp:
-                            response = await upload_resp.text()
-                            print(json.loads(response))
-                            result = json.loads(response)["result"]
-                            print(f"云端扫描结果为：{result}")
-                            if result != "":
-                                break
-                        sleep(3)
+    try:
+        conn = aiohttp.TCPConnector(ssl=False)
+        async with aiohttp.ClientSession(connector=conn) as session:
+            headers = {"Content-Type": "application/json"}
+            data = {"webshell_text": str(base64.b64encode(bytes(file_text, encoding='utf-8')).decode())}
+            # print(data)
+            # print(url+"/api/v1/anonymous_see_file?apikey="+apikey+"&md5="+file_md5)
 
+            async with session.get(url + "/api/v1/anonymous_see_file?apikey=" + apikey + "&md5=" + file_md5, json=data,
+                                   headers=headers) as check_resp:
+                print("正在进行历史结果探测")
+                resp = await check_resp.text()
+                result = json.loads(resp)["result"]
+                if result != "":
+                    print(f"云端结果已存在,云端扫描结果为：{result}")
+                else:
+                    print("云端结果不存在,开始上传")
+                    async with session.post(url + "/api/v1/anonymous_up_file?apikey=" + apikey + "&md5=" + file_md5,
+                                            json=data, headers=headers) as resp:
+                        print(resp.status)
+                        print(await resp.json())
+                        print("文件上传成功")
+                        print()
+                        for i in range(0, 10):
+                            print("正在等待云端扫描返回结果")
+                            async with session.get(url + "/api/v1/anonymous_see_file?apikey=" + apikey + "&md5=" + file_md5,
+                                                   json=data, headers=headers) as upload_resp:
+                                response = await upload_resp.text()
+                                print(json.loads(response))
+                                result = json.loads(response)["result"]
+                                print(f"云端扫描结果为：{result}")
+                                if result != "":
+                                    break
+                            sleep(3)
+    except Exception as e:
+        print(f"出现错误：{e}")
+        pass
 
 async def list_files(dir_path):
     for root, dirs, files in os.walk(dir_path):
         for f in files:
             print(os.path.join(root, f))
-            await upload_file(calculatefile_md5(file_text(os.path.join(root, f))), file_text(os.path.join(root, f)))
+            await upload_file(calc_md5(file_text(os.path.join(root, f))), file_text(os.path.join(root, f)))
 
 
 async def get_type(filepath):
@@ -60,7 +70,7 @@ async def get_type(filepath):
             await list_files(filepath)
         else:
             print("{} 是一个文件".format(filepath))
-            await upload_file(calculatefile_md5(file_text(filepath)), file_text(filepath))
+            await upload_file(calc_md5(file_text(filepath)), file_text(filepath))
             # file_text(path)
     else:
         print("{} 不存在或者不是一个文件或文件夹".format(filepath))
@@ -69,10 +79,11 @@ async def get_type(filepath):
 def file_text(path):
     try:
         text = open(path, 'r', encoding='utf-8').read()
-        # print(text)
+        return text
     except Exception as e:
         print(f"出现错误：{e}")
-    return text
+        pass
+
 
 # get_type(path)
 
