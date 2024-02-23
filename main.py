f5c7214563abaf6dfb6a09d97d4475006ca7ae6b
caojian <1736665642@qq.com>
2024-02-23 16:14:01 +0800

    Update.

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import asyncio
 
 async def main():
     parser = argparse.ArgumentParser(description="一个简单的命令行参数解析示例")
-    parser.add_argument("-CloudScan", help="是否启用云端扫描", default="Ture")
-    parser.add_argument("--path", '-p', help="扫描文件的路径")
+    parser.add_argument("--CloudScan", help="是否启用云端扫描", default="True", type=bool)
+    parser.add_argument("--path", '-p', help="扫描文件的路径",type=str)
     args = parser.parse_args()
     await Scan.get_type(args.path)
 
