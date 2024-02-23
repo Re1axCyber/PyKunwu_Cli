38ca8c0317dcae9452b4c4894cf5770e1896ec56
caojian <1736665642@qq.com>
2024-02-23 15:26:36 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,2 @@
+url="https://ti.moresec.cn"
+apikey="13d28c00f252ddde4371c56a4e2f47ab8ab718b5f81e2f63d82f8d974cc78b07"
---
+++ main.py
@@ -0,0 +1,20 @@
+import argparse
+from logo import *
+from Scripts import Scan
+import asyncio
+
+
+async def main():
+    parser = argparse.ArgumentParser(description="一个简单的命令行参数解析示例")
+    parser.add_argument("-CloudScan", help="是否启用云端扫描", default="Ture")
+    parser.add_argument("--path", '-p', help="扫描文件的路径")
+    args = parser.parse_args()
+    await Scan.get_type(args.path)
+
+
+if __name__ == '__main__':
+
+    print(kw_logo)
+    print("--------------------------start----------------------------\n")
+    asyncio.run(main())
+    print("--------------------------end----------------------------\n")
